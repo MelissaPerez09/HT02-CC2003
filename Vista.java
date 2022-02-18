dcd0ac9f1087b95a269f4ca01be74883b7fccf8b
Melissa Pérez <84192851+MelissaPerez09@users.noreply.github.com>
2022-02-17 22:06:43 -0600

    Creación de clase

---
+++ Vista.java
@@ -0,0 +1,21 @@
+/** 
+ * Nombre: Emily ELvia Melissa Perez Alarcon
+ * Carnet: 21385
+ * Curso: Programacion orientada a objetos
+ * Nombre del programa: Vista.java
+    @version: 
+        - Creacion: 17/02/2022
+        - Ultima modificacion: 00/02/2022
+    Clase sistema para inputs y outputs
+*/
+
+/**
+ * @author melissaperez_
+ *
+*/
+
+import java.util.Scanner;
+
+public class Vista{
+  
+}

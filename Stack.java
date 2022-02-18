bc89657bb2f2a89f6930b0861ad724cd313f7c90
Melissa Pérez <84192851+MelissaPerez09@users.noreply.github.com>
2022-02-17 22:49:03 -0600

    Creación de clase

---
+++ Stack.java
@@ -0,0 +1,21 @@
+/** 
+ * Nombre: Emily ELvia Melissa Perez Alarcon
+ * Carnet: 21385
+ * Curso: Algoritmos y estructuras de datos
+ * Nombre del programa: Stack.java
+    @version: 
+        - Creacion: 17/02/2022
+        - Ultima modificacion: 00/02/2022
+    Clase que implementa los stacks por medio de vectores
+*/
+
+/**
+ * @author melissaperez_
+ *
+*/
+
+import java.util.Vector;
+
+public class Stack<T> implements IStack<T>{
+  
+}

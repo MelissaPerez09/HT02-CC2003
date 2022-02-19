d5915186eed90d9e12058feec9da24f75bfb536f
Melissa Pérez <84192851+MelissaPerez09@users.noreply.github.com>
2022-02-18 22:20:55 -0600

    pre final version

--- Principal.java
+++ Principal.java
@@ -5,7 +5,7 @@
  * Nombre del programa: Calculadora.java
     @version: 
         - Creacion: 17/02/2022
-        - Ultima modificacion: 00/02/2022
+        - Ultima modificacion: 18/02/2022
     Clase que implementa la calculadora
 */
 
@@ -14,6 +14,41 @@
  *
 */
 
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Principal{
-  
+
+    private static ArrayList<String> leerArchivo(){
+        Vista vis = new Vista();
+        try{
+            File datos = new File("datos.txt");
+            ArrayList<String> archivoDatos = new ArrayList<String>();
+            Scanner entrada = new Scanner(datos);
+            while(reader.hasNextLine()){
+                archivoDatos.add(reader.nextLine());
+            }
+            reader.close();
+            return archivoDatos;
+        }
+        catch(FileNotFoundException a){
+            vis.error();
+            return null;
+        }
+    }
+
+    public static void main(String[] args){
+        Vista vis = new Vista();
+        Calculadora calc = new Calculadora();
+        vis.bienvenida();
+        for(int i=0; i < leerArchivo().size(); i++){
+            int resultado = calc.operar(leerArchivo().get(i));
+            if(resultado != 000){
+                vis.mostrarResultado();
+            }
+            vis.despedida();
+        }
+    }
 }
